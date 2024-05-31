89b7c9b8a32b38606e9021667429860e45c7a951
Eber_C <ghack7756@gmail.com>
2024-05-30 22:22:26 -0400

    modificacion de apariencia, variables, fuentes

--- login/login2.py
+++ login/login2.py
@@ -1,53 +1,80 @@
 import tkinter as tk
 import customtkinter as CustomTK
+import variables as var
 from PIL import ImageTk, Image
 
 class LoginApp:
     def __init__(self):
         # definicion de aparciencia
-        CustomTK.set_appearance_mode("dark")
-        CustomTK.set_default_color_theme("green")
+        CustomTK.set_appearance_mode("light")
+        CustomTK.set_default_color_theme("dark-blue")
         # creando ventana
         self.app = CustomTK.CTk()
         self.app.geometry("1024x720")
         self.app.title("Login")
         
-        # importando y añadiendo imagen de fondo
-        img_bg = ImageTk.PhotoImage(Image.open("imagen/background_login.jpg"))    
+        # importando y añadiendo imagenes de background y iconos
+        img_bg = ImageTk.PhotoImage(Image.open("imagen/background_light.png"))    
+        img_login_left = CustomTK.CTkImage(Image.open("imagen/interface_control.png"), size=(240,240))
+        img_bg_frame = ImageTk.PhotoImage(Image.open("imagen/background_gradient_blue.png"))
+        
+        # frame principal
         self.div_login = CustomTK.CTkLabel(master=self.app, image=img_bg)
         self.div_login.pack()
 
-        # añadiendo frame right
-        self.frame = CustomTK.CTkFrame(master=self.div_login, width=320, height=380,corner_radius=50)
-        self.frame.place(relx=0.5, rely=0.5, anchor=tk.E) #posicion del frame
-
         # añadiendo frame left
-        self.frame2 = CustomTK.CTkFrame(master=self.div_login, width=320,height=380,fg_color="#151515")
-        self.frame2.place(relx=0.5,rely=0.5,anchor=tk.W) #posicion del frame
+        self.frame = CustomTK.CTkFrame(master=self.div_login,
+                                        width=663,
+                                        height=546)
         
-        # imagen de frame2
-        img_login_left = CustomTK.CTkImage(Image.open("imagen/logo.png"), size=(320,320))
-        image_label=CustomTK.CTkLabel(master=self.frame2,image=img_login_left,text=" ")
-        image_label.place(x=15, y=30)
+        self.frame.place(relx=0.58, rely=0.5, anchor=tk.E) #posicion del frame
         
-        # imagen de login user
-        img_log = CustomTK.CTkImage(Image.open("imagen/user_login.png"), size=(80,80))
-        image_label = CustomTK.CTkLabel(master=self.frame,image=img_log, text=" ")
-        image_label.place(x=125, y=25)
+        img_bg = CustomTK.CTkLabel(master=self.frame,
+                                    image=img_bg_frame,
+                                    text=" ")
+        img_bg.place(x=0,y=0)
+
+        
+        # añadiendo frame right
+        self.frame2 = CustomTK.CTkFrame(master=self.div_login,
+                                        width=457,
+                                        height=546,
+                                        fg_color="#FFFFFF")
+        
+        self.frame2.place(relx=0.58,rely=0.5,anchor=tk.W) #posicion del frame
+    
+        # # imagen de login user
+        # img_log = CustomTK.CTkImage(Image.open("imagen/user.png"), size=(100,100))
+        # image_label = CustomTK.CTkLabel(master=self.frame2,image=img_log, text=" ")
+        # image_label.place(x=115, y=25)
 
         # añadiendo texto
-        div_login_two = CustomTK.CTkLabel(master=self.frame,text="Iniciar sesion",font=("Lucida Sans", 24))
-        div_login_two.place(x=85, y=110)
+        div_login_two = CustomTK.CTkLabel(master=self.frame2,
+                                        text="Iniciar sesion",
+                                        font=var.font_text_2)
+        div_login_two.place(x=85, y=115)
 
         # entradas de texto
-        self.input_one = CustomTK.CTkEntry(master=self.frame, width=220, placeholder_text="Usuario", font=("Verdana",16))
+        self.input_one = CustomTK.CTkEntry(master=self.frame2, width=220,
+                                        placeholder_text="Usuario",
+                                        font=("Verdana",16))
+        
         self.input_one.place(x=50, y=160)
 
-        self.input_two = CustomTK.CTkEntry(master=self.frame, width=220, placeholder_text="Contraseña", show="*", font=("Verdana",16))
+        self.input_two = CustomTK.CTkEntry(master=self.frame2,
+                                        width=220,
+                                        placeholder_text="Contraseña",
+                                        show="*",
+                                        font=("Verdana",16))
+        
         self.input_two.place(x=50, y=200)
 
         # boton
-        button1 = CustomTK.CTkButton(master=self.frame, width=220, text="Login", corner_radius=6)
+        button1 = CustomTK.CTkButton(master=self.frame2,
+                                    width=220,
+                                    text="Login",
+                                    corner_radius=6)
+        
         button1.place(x=50, y=270)
 
 
---
+++ login/variables.py
@@ -0,0 +1,12 @@
+# -----------Variables de colores--------
+text_white = "#FFFFFF"
+text_black = "#000000"
+bg_light = "#8CEE4"
+bg_dark = "#161616"
+blue_button = "#0575E6"
+black_button = "#000000"
+
+# ---------variables de fuentes----------------
+
+font_text = ("CantoraOne",16)
+font_text_2 = ("Candal",24)
