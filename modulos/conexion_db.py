c146b37f3d09354660ced91c936ca868f7649540
Eber_C <ghack7756@gmail.com>
2024-06-10 14:25:16 -0400

    nuvos requeriments y modificacion en base de datos

--- modulos/conexion_db.py
+++ modulos/conexion_db.py
@@ -6,27 +6,16 @@ conn = sqlite3.connect('./bd_rufino/bd_escuela.db')
 # Crear un cursor para interactuar con la base de datos
 cursor = conn.cursor()
 
-# Ejemplo de consulta de datos
-cursor.execute("""
-               SELECT
-p.id_personal,
-p.primer_nombre,
-p.segundo_nombre,
-p.primer_apellido,
-p.segundo_apellido,
-p.genero,
-g.grado
-FROM
-Personal p
-JOIN
-Docente d ON p.id_personal = d.id_personal
-JOIN
-Grado g ON d.id_grado = g.id_grado;
-""")
-filas = cursor.fetchall()
+# Ejemplo de consulta de datos (reemplaza 'nombre_tabla' con el nombre real de tu tabla)
+consulta_sql = "Select * TABLES"
 
-for fila in filas:
-    print(fila)
+try:
+    cursor.execute(consulta_sql)
+    filas = cursor.fetchall()
+    for fila in filas:
+        print(fila)
+except sqlite3.Error as e:
+    print(f"Error al ejecutar la consulta: {e}")
 
 # Cerrar la conexión
 conn.close()
