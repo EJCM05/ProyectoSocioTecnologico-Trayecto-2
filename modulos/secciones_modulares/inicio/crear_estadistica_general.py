494ca08d5804ede23a5e38aad7e04405d7fa593a
kinggtx <kinggtx777@gmail.com>
2024-06-24 18:36:21 -0400

    grafica colores

--- modulos/secciones_modulares/inicio/crear_estadistica_general.py
+++ modulos/secciones_modulares/inicio/crear_estadistica_general.py
@@ -1,27 +1,26 @@
-# codigo nuevo
 import matplotlib.pyplot as plt
-
-
-import matplotlib.pyplot as plt
+from modulos.variables import variables as var
 
 def crear_estadistica_general(lista_datos):
-     nombres_datos = ['Simoncito', 'Inicial A', 'Inicial B', 'Inicial C', '1er Grado', '2do Grado', '3er Grado', '4to Grado', '5to Grado', '6to Grado']
+    nombres_datos = ['Simoncito', 'Inicial A', 'Inicial B', 'Inicial C', '1er Grado', '2do Grado', '3er Grado', '4to Grado', '5to Grado', '6to Grado']
 
-     # Crear una figura y un eje
-     fig, ax = plt.subplots()
-     ax.plot(lista_datos, marker='o', linestyle='-', color='b')
-     ax.set_xlabel('Índice')
-     ax.set_ylabel('Cantidad de Estudiantes')
-     ax.set_title('Número de Estudiantes por Grado')
+    # Colores para cada barra (puedes personalizar esta lista)
+    colores = [var.btn_gray, var.btn_pink, var.btn_beige, var.btn_gold, var.btn_blue, var.btn_red_black, var.btn_purple, var.btn_green, var.btn_lila, var.btn_blueosc]
 
-     # Establecer las etiquetas en el eje x
-     ax.set_xticks(range(len(lista_datos)))
-     ax.set_xticklabels(nombres_datos, rotation=20)
+    # Crear una figura y un eje
+    fig, ax = plt.subplots()
+    ax.bar(nombres_datos, lista_datos, color=colores)
+    ax.set_xlabel('Grado')
+    ax.set_ylabel('Cantidad de Estudiantes')
+    ax.set_title('Número de Estudiantes por Grado')
 
-     # Agregar etiquetas a los puntos
-     for i, valor in enumerate(lista_datos):
-         ax.annotate(f'{valor}', (i, valor), textcoords="offset points", xytext=(10, -10), ha='center')
+    # Rotar las etiquetas en el eje x para mayor legibilidad
+    plt.xticks(rotation=20)
 
-     # Guardar la figura como una imagen
-     fig.savefig('matricula_pdf_img/img_estadistica_general/estadistica_general.png')
-     plt.close(fig)
+    # Agregar etiquetas a las barras
+    for i, valor in enumerate(lista_datos):
+        ax.text(i, valor, str(valor), ha='center', va='bottom')
+
+    # Guardar la figura como una imagen
+    fig.savefig('matricula_pdf_img/img_estadistica_general/estadistica_general.png')
+    plt.close(fig)
